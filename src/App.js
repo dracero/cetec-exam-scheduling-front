388d95941b8c8f24df5259dd241a0d9de1d01134
Manuel Collazo <collazo.manuel.28@gmail.com>
2022-07-28 23:17:02 -0300

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 // import logo from './logo.svg';
 import './App.css';
 
-import { React, useCallback } from "react";
-import { useSelector } from "react-redux";
+import { React, useCallback, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Courses from './courses';
+import * as courseActions from "./app/actions/CourseActions";
 import Start from './start';
+import * as startActions from "./app/actions/StartActions";
 import Finish from './finish';
+import * as finishActions from "./app/actions/FinishActions";
 import StartMinutesMargins from './startMinutesMargins';
+import * as startMinutesMarginsActions from "./app/actions/StartMinutesMarginActions";
 import FinishMinutesMargins from './finishMinutesMargins';
+import * as finishMinutesMarginsActions from "./app/actions/FinishMinutesMarginActions";
 
 import axios from "axios";
 
 import logo from './logo.svg';
 
+import Alert from "@mui/material/Alert";
+
 function App() {
+
+  const [state, setState] = useState('');
 
   const course = useSelector((store) => store.course.course);
   const start = useSelector((store) => store.start.start);
@@ -21,10 +30,19 @@ function App() {
   const startMinutesMargin = useSelector((store) => store.startMinutesMargin.startMinutesMargin);
   const finishMinutesMargin = useSelector((store) => store.finishMinutesMargin.finishMinutesMargin);
 
+  const dispatch = useDispatch();
+
+  const clearInputs = () => {
+    dispatch(courseActions.course(""));
+    dispatch(startActions.start(""));
+    dispatch(finishActions.finish(""));
+    dispatch(startMinutesMarginsActions.startMinutesMargin(""));
+    dispatch(finishMinutesMarginsActions.finishMinutesMargin(""));
+  };
+
   const upload = useCallback(() => {
 
     const data = {
-      email:"mail88@test.com",
       course: course,
       start: start,
       finish: finish,
@@ -32,8 +50,21 @@ function App() {
       finishMinutesMargin: finishMinutesMargin
     }
 
+    if (
+      data.course === "" ||
+      data.start === "" ||
+      data.finish === "" ||
+      data.startMinutesMargin === "" ||
+      data.finishMinutesMargin === ""
+    ) {
+      console.log("ERROR");
+      setState('Error');
+      return;
+    }
+
+
+
     var bodyFormData = new FormData();
-    bodyFormData.append('email', data.email);
     bodyFormData.append('course', data.course);
     bodyFormData.append('start', data.start);
     bodyFormData.append('finish', data.finish);
@@ -50,10 +81,14 @@ function App() {
       .then(function (response) {
         //handle success
         console.log("EXITO");
+        setState('Success');
+        clearInputs();
       })
       .catch(function (response) {
         //handle error
+        
         console.log("ERROR");
+        setState('Error');
       });
 
   }, [course, start, finish, startMinutesMargin, finishMinutesMargin]);
@@ -69,6 +104,20 @@ function App() {
         <StartMinutesMargins />
         <FinishMinutesMargins />
         <button onClick={upload}> Agregar nuevo examen </button>
+        {(state === 'Success') &&
+          <div>
+            <Alert variant="outlined" severity="success">
+              Examen agregado exitosamente.
+            </Alert>
+          </div>
+        }
+        {(state === 'Error') &&
+          <div>
+            <Alert variant="outlined" severity="error">
+              Error
+            </Alert>
+          </div>
+        }
       </header>
     </div>
   );
